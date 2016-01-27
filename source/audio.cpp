548a9ceb670b68d06957bdc883c2415c738c8c36
Trevor Henderson <tfh@mit.edu>
2016-01-27 13:35:04 -0500

    updated audio class

--- source/audio.cpp
+++ source/audio.cpp
@@ -24,17 +24,9 @@ double quinnsSecondEstimator(int k, fftw_complex * fft) {
     std::complex<double> past(fft[k - 1][0], fft[k - 1][1]);
     std::complex<double> future(fft[k + 1][0], fft[k + 1][1]);
     
-
     double alpha1 = (past/present).real();
     double alpha2 = (future/present).real();
 
-    //double squaredMagnitude = fft[k][0]* fft[k][0] + fft[k][1]* fft[k][1];
-    
-    //double alpha1 = (fft[k - 1][0]*fft[k][0] + fft[k - 1][1]*fft[k][1])
-                    ///squaredMagnitude; //Re(fft[k-1]/fft[k])
-    //double alpha2 = (fft[k + 1][0]*fft[k][0] + fft[k + 1][1]*fft[k][1])
-                    ///squaredMagnitude; //
-    
     double delta1 = alpha1/(1 - alpha1);
     double delta2 = -alpha2/(1 - alpha2);
     
@@ -45,35 +37,30 @@ double quinnsSecondEstimator(int k, fftw_complex * fft) {
     return delta;
 }
 
-void computeSpectrogram(double * in, int size, UserData * u) {
+void AudioStream::computeSpectrogram() {
 
-  Visualizer * v = u -> visualizer;
-
-  for (int i = 0; i < SPEC_SIZE; i++) v -> setSpec(i, 0);
-    
-  fftw_complex * fftOut = new fftw_complex[size/2 + 1];
+  for (int i = 0; i < SPEC_SIZE; i++) visualizer -> setSpec(i, 0);
   
-  fftw_plan plan = fftw_plan_dft_r2c_1d(size, in, fftOut, FFTW_ESTIMATE);
-  
-  fftw_execute(plan);
-  
-  fftw_destroy_plan(plan);
+  fftw_execute(fftPlan);
 
   //Fill with magnitudes of peak values
-  for (int bin = 2; bin < size/2; bin++) {
+  for (int bin = 2; bin < SIZE/2; bin++) {
       
-      double leftMag = fftOut[bin-1][0]*fftOut[bin-1][0]+fftOut[bin-1][1]*fftOut[bin-1][1];
-      double midMag = fftOut[bin][0]*fftOut[bin][0]+fftOut[bin][1]*fftOut[bin][1];
-      double rightMag = fftOut[bin+1][0]*fftOut[bin+1][0]+fftOut[bin+1][1]*fftOut[bin+1][1];
+      double leftMag = fft[bin-1][0]*fft[bin-1][0]+fft[bin-1][1]*fft[bin-1][1];
+      double midMag = fft[bin][0]*fft[bin][0]+fft[bin][1]*fft[bin][1];
+      double rightMag = fft[bin+1][0]*fft[bin+1][0]+fft[bin+1][1]*fft[bin+1][1];
       
       if ((midMag >rightMag) && (midMag > leftMag)) {
           
-          double delta = quinnsSecondEstimator(bin,fftOut);
+          double delta = quinnsSecondEstimator(bin,fft);
           
           if ( fabs(delta) < 1) {
               double peakBin = bin + delta;
               
-              double peakFreq = peakBin * 96000/size;
+              double peakFreq = peakBin * 
+                (Pa_GetDeviceInfo(inputParameters.device) -> defaultSampleRate)
+                /SIZE;
+
               double freqMod =fmod(log2(peakFreq), 1);
               if (freqMod < 0 ) {
                   freqMod += 1;
@@ -87,43 +74,63 @@ void computeSpectrogram(double * in, int size, UserData * u) {
               double rightPercent = desired - leftNote;
               double leftPercent = 1 - rightPercent;
               
-              double rightAmp = rightPercent * sqrt(fftOut[bin][0] * fftOut[bin][0] + fftOut[bin][1] * fftOut[bin][1]);
-              double leftAmp = leftPercent * sqrt(fftOut[bin][0] * fftOut[bin][0] + fftOut[bin][1] * fftOut[bin][1]);
+              double rightAmp = rightPercent * sqrt(fft[bin][0] * fft[bin][0] + fft[bin][1] * fft[bin][1]);
+              double leftAmp = leftPercent * sqrt(fft[bin][0] * fft[bin][0] + fft[bin][1] * fft[bin][1]);
               
 
-              v -> setSpec(leftNote, v -> getSpec(leftNote) + leftAmp);
-              v -> setSpec(rightNote, v -> getSpec(rightNote) + rightAmp);
+              visualizer -> setSpec(leftNote, visualizer -> getSpec(leftNote) + leftAmp);
+              visualizer -> setSpec(rightNote, visualizer -> getSpec(rightNote) + rightAmp);
           }
       }
     }
 
     //Find max of spectrogram
-    double max = v -> getSpec(0);
+    double max = visualizer -> getSpec(0);
     for (int i = 0; i < SPEC_SIZE; i++) {
-      if ( v -> getSpec(i) > max) {
-        max = v -> getSpec(i);
+      if ( visualizer -> getSpec(i) > max) {
+        max = visualizer -> getSpec(i);
       }
     }
 
 
-    u -> normlizationFactor += (max - u -> prevMaxValue);
-    u -> prevMaxValue = max;
+    normalizationFactor += (max - previousMaxValue);
+    previousMaxValue = max;
 
     //Normalize spectrogram using max
     for (int i = 0; i < SPEC_SIZE; i++) {
-      if (u -> normlizationFactor != 0) {
-        v -> setSpec(i, (v -> getSpec(i))/(u -> normlizationFactor));
+      if (normalizationFactor != 0) {
+        visualizer -> setSpec(i, (visualizer -> getSpec(i))/(normalizationFactor));
       }
     }
 
-    v -> updateWindow();
+    visualizer -> updateWindow();
 }
 
-double hamming(int i, int N) {
-  return 0.54 - 0.46 * cos( 2 * M_PI * i/(double) (N -1));
+double AudioStream::hammingWindow(int i) {
+  return 0.54 - 0.46 * cos( 2 * M_PI * i/(double) (FRAMES_PER_BUFFER * OVERLAP));
 }
 
-static int visualizerCallBack (
+void AudioStream::processNewSamples ( float * newSamples ) {
+
+  // Shift samples over
+  for (int i = 0; i < FRAMES_PER_BUFFER * (OVERLAP - 1); i++) {
+    sampleBuffer[i] = sampleBuffer[i + FRAMES_PER_BUFFER];
+  }
+
+  // Add new samples
+  for (int i = 0; i < FRAMES_PER_BUFFER; i++) {
+    sampleBuffer[i + FRAMES_PER_BUFFER * (OVERLAP -1)] = newSamples[i];
+  }
+
+  // Window samples
+  for (int i = 0; i < FRAMES_PER_BUFFER * OVERLAP; i++) {
+    windowedBuffer[i] = hammingWindow(sampleBuffer[i]);
+  }
+
+  this -> computeSpectrogram();
+}
+
+int AudioStream::visualizerCallback (
     const void * inputBuffer,
     void * outputBuffer,
     unsigned long framesPerBuffer,
@@ -133,104 +140,102 @@ static int visualizerCallBack (
 
   if ( inputBuffer != NULL ) {
 
+    AudioStream * s = (AudioStream *) userData;
+
     float ** channels = ( float ** ) inputBuffer;
 
-    float * in_ = channels[0];
-
-    double in[framesPerBuffer * OVERLAP];
-
-    UserData * u = (UserData *) userData;
-
-    double * prev = u -> previousSamples;
-
-    for (int i = 0; i < FRAMES_PER_BUFFER * (OVERLAP - 1); i++) {
-      in[i] = prev[i + FRAMES_PER_BUFFER] * hamming(i, FRAMES_PER_BUFFER * OVERLAP);
-      prev[i] = prev[i + FRAMES_PER_BUFFER];
-    }
-
-    for (int i = 0; i < framesPerBuffer; i++) {
-      int j = i + framesPerBuffer * (OVERLAP -1);
-      in[j] = in_[i] * hamming(j, FRAMES_PER_BUFFER * OVERLAP);
-      prev[j] = in_[i];
-    }
-
-    computeSpectrogram( in, FRAMES_PER_BUFFER * OVERLAP, u );
-
+    float * input = channels[0];
+    
   }
 
   return paContinue;
 
 }
 
-portAudioSettings portAudioInit() {
-  portAudioSettings s;
-  PaError err;
-
+AudioStream::AudioStream() {
   err = Pa_Initialize();
-  if( err != paNoError ) portAudioError(err);
+  if( err != paNoError ) this -> streamError();
 
   int numDevices = Pa_GetDeviceCount();
-
-  if (numDevices ==0) portAudioError(err);
-
-  std::cout << "Available devices: " << std::endl;
-
-  for (int i =0; i < numDevices; i++) {
-    s.deviceInfo  = Pa_GetDeviceInfo( i );
-    std::cout << i << ".\t" << s.deviceInfo -> name << std::endl;
+  if (numDevices ==0) {
+    std::cout << "No available audio devices!" << std::endl;
+    this -> streamError();
   }
 
+  // List Available devices
+  std::cout << "Available devices: " << std::endl;
+  for (int i =0; i < numDevices; i++) {
+    std::cout << i << ".\t" << 
+      Pa_GetDeviceInfo(i) -> name << std::endl;
+  }
+
+  // Mark default device
   std::cout << "Device (default = " << 
     Pa_GetDefaultInputDevice() << ". " <<
     Pa_GetDeviceInfo( Pa_GetDefaultInputDevice() ) -> name << 
     "): ";
 
   int choice;
-  std::cin >> choice; 
+  std::string choice_;
+  std::getline(std::cin, choice_);
+  try {
+    choice = std::stoi(choice_);
+  } catch (int e) {
+    choice = Pa_GetDefaultInputDevice();
+  }
 
   if (choice < 0 || choice > numDevices) {
     choice = Pa_GetDefaultInputDevice();
   }
 
-  s.inputParameters.device = choice;
-  s.deviceInfo = Pa_GetDeviceInfo( choice );
+  inputParameters.device = choice;
 
-  std::cout << "Using device " << s.deviceInfo -> name << std::endl;
+  if (inputParameters.device == paNoDevice) {
+    std::cout << "Device " << choice << " not found" << std::endl;
+    this -> streamError();
+  }
+
+  std::cout << "Using device " << Pa_GetDeviceInfo(choice)-> name << std::endl;
     
-  s.inputParameters.channelCount = s.deviceInfo -> maxInputChannels;
-  s.inputParameters.sampleFormat = paFloat32 | paNonInterleaved;
-  s.inputParameters.suggestedLatency = s.deviceInfo -> defaultLowInputLatency;
-  s.inputParameters.hostApiSpecificStreamInfo = NULL;
+  inputParameters.channelCount = Pa_GetDeviceInfo(choice) -> maxInputChannels;
+  inputParameters.sampleFormat = paFloat32 | paNonInterleaved;
+  inputParameters.suggestedLatency = Pa_GetDeviceInfo(choice) -> defaultLowInputLatency;
+  inputParameters.hostApiSpecificStreamInfo = NULL;
 
-  s.outputParameters.device = Pa_GetDefaultOutputDevice();
-  s.outputParameters.channelCount = s.deviceInfo -> maxInputChannels;
-  s.outputParameters.sampleFormat = paFloat32;
-  s.outputParameters.suggestedLatency = Pa_GetDeviceInfo( s.outputParameters.device ) -> defaultLowOutputLatency;
-  s.outputParameters.hostApiSpecificStreamInfo = NULL;
+  outputParameters.device = Pa_GetDefaultOutputDevice();
+  outputParameters.channelCount = Pa_GetDeviceInfo(outputParameters.device) -> maxOutputChannels;
+  outputParameters.sampleFormat = paFloat32;
+  outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device ) -> defaultLowOutputLatency;
+  outputParameters.hostApiSpecificStreamInfo = NULL;
 
-  return s;
+  fftw_plan plan = fftw_plan_dft_r2c_1d(SIZE, 
+                                        windowedBuffer, 
+                                        fft,
+                                        FFTW_ESTIMATE);
 }
 
-void portAudioStartStream( portAudioSettings s, void * userData ) {
+void AudioStream::startStream( Visualizer * v ) {
+
+  visualizer = v;
 
   PaError err = Pa_OpenStream (
-      &(s.stream),
-      &(s.inputParameters),
-      &(s.outputParameters),
-      s.deviceInfo -> defaultSampleRate,
+      &stream,
+      &inputParameters,
+      &outputParameters,
+      Pa_GetDeviceInfo(inputParameters.device) -> defaultSampleRate,
       FRAMES_PER_BUFFER,
       0,
-      visualizerCallBack,
-      userData
+      visualizerCallback,
+      this
       );
 
-  if ( err != paNoError ) portAudioError(err);
+  if ( err != paNoError ) this -> streamError();
 
-  err = Pa_StartStream( s.stream );
-  if ( err != paNoError ) portAudioError(err);
+  err = Pa_StartStream( stream );
+  if ( err != paNoError ) this -> streamError();
 }
 
-void portAudioError(PaError err) {
+void AudioStream::streamError() {
   Pa_Terminate();
   std::cerr << Pa_GetErrorText( err ) << std::endl;
   throw 20;
--- source/audio.hpp
+++ source/audio.hpp
@@ -3,34 +3,57 @@
 
 #define FRAMES_PER_BUFFER 1024
 #define OVERLAP 8
+#define SIZE (FRAMES_PER_BUFFER * OVERLAP)
 
 #include <portaudio.h>
 #include <fftw3.h>
 
 
-double quinnKappa(double in);
+class AudioStream {
+  private:
+    Visualizer * visualizer;
 
-double quinnsSecondEstimator(int k, fftw_complex * fft);
+    PaStreamParameters inputParameters, outputParameters;
+    PaStream * stream;
+    PaError err;
 
-void computeSpectrogram(double * in, int size, Visualizer * v);
+    double normalizationFactor;
+    double previousMaxValue;
 
-struct portAudioSettings {
-  PaStreamParameters inputParameters, outputParameters;
-  PaStream * stream;
-  const PaDeviceInfo * deviceInfo;
+    double sampleBuffer[SIZE];
+    double windowedBuffer[SIZE];
+
+    fftw_complex fft[SIZE/2 + 1];
+    fftw_plan fftPlan;
+
+    // ^^needs destructor!
+  
+  public:
+
+    AudioStream();
+
+    void startStream(Visualizer * v);
+
+    static int visualizerCallback (
+        const void * inputBuffer,
+        void * outputBuffer,
+        unsigned long framesPerBuffer,
+        const PaStreamCallbackTimeInfo * timeInfo,
+        PaStreamCallbackFlags statusFlags,
+        void * userData );
+
+    void processNewSamples( float * newSamples );
+
+    double hammingWindow(int i);
+
+    void computeSpectrogram();
+
+    double quinnKappa(double in);
+
+    double quinnsSecondEstimator(int k, fftw_complex * fft);
+
+    void streamError();
+
 };
-
-struct UserData {
-  Visualizer * visualizer;
-  double normlizationFactor;
-  double prevMaxValue;
-  double previousSamples[FRAMES_PER_BUFFER * OVERLAP];
-};
-
-portAudioSettings portAudioInit();
-
-void portAudioStartStream(portAudioSettings s, void * userData);
-
-void portAudioError( PaError err );
 
 #endif
